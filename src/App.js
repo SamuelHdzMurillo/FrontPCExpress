f1b4165ab12b12e7836003423fb26ce479234ae6
unknown <samuelop99op@gmail.com>
2023-07-05 13:47:46 -0700

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
-function App() {
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import TextField from '@mui/material/TextField';
+
+import LoginPage from './LoginPage';
+
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              PC Express SAS
+            </Typography>
+            <Button component={Link} to="/" color="inherit">
+              Inicio
+            </Button>
+            <Button component={Link} to="/login" color="inherit">
+              Iniciar Sesión
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Box sx={{ p: 2 }}>
+          <TextField label="Buscar" variant="outlined" fullWidth />
+
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: '20px' }}>
+            <Card sx={{ maxWidth: 345, margin: '10px' }}>
+              <CardMedia component="img" height="140" image="https://via.placeholder.com/150" alt="Product" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Producto 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Descripción del producto.
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+                  <Button variant="outlined" size="small">
+                    Detalle
+                  </Button>
+                  <Typography variant="subtitle1" color="text.secondary">
+                    Estado: Activo
+                  </Typography>
+                </Box>
+              </CardContent>
+            </Card>
+
+            <Card sx={{ maxWidth: 345, margin: '10px' }}>
+              <CardMedia component="img" height="140" image="https://via.placeholder.com/150" alt="Product" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Producto 2
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Descripción del producto.
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+                  <Button variant="outlined" size="small">
+                    Detalle
+                  </Button>
+                  <Typography variant="subtitle1" color="text.secondary">
+                    Estado: Inactivo
+                  </Typography>
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+        </Box>
+
+        <Switch>
+          <Route path="/login">
+            {isLoggedIn ? <Redirect to="/" /> : <LoginPage />}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Página de Inicio de Sesión</h1>
+      {/* Aquí puedes agregar tu formulario de inicio de sesión */}
+    </div>
+  );
+};
+
+export default LoginPage;
